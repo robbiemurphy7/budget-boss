784db64b8c350dfdd9fe10793ae0edb640eb871c
robbiemurphy <rmurphy987@gmail.com>
2022-05-31 03:51:37 -0500

    manifest.json

---
+++ public/servicer.js
@@ -0,0 +1,22 @@
+const APP_PREFIX = 'Budget';     
+const VERSION = 'version_01';
+const CACHE_NAME = APP_PREFIX + VERSION;
+const FILES_TO_CACHE = [
+    "./index.html",
+    "./css/styles.css",
+    "./js/index.js",
+    "./js/idb.js",
+    "icons/icon-144x144.png",
+    "icons/icon-512x512.png"
+  ];
+
+
+
+  self.addEventListener('install', function (e) {
+    e.waitUntil(
+      caches.open(CACHE_NAME).then(function (cache) {
+        console.log('installing cache : ' + CACHE_NAME)
+        return cache.addAll(FILES_TO_CACHE)
+      })
+    )
+})
